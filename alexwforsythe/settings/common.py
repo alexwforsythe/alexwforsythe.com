4f9a388da9ca4699150519552b54c2f5d8071f63
Alex Forsythe <alexwforsythe@gmail.com>
2014-03-25 20:54:48 -0500

    commenting STATICFILES_DIRS until needed

--- alexwforsythe/settings/common.py
+++ alexwforsythe/settings/common.py
@@ -97,6 +97,6 @@ USE_TZ = True
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
